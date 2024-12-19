1f12c3cd0a431f9343aa2291bfbd09ad3a5b7db3
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-12-19 18:41:28 -0300

    feat: transformando uniquedigit em tablea

--- src/main/java/br/com/desafio/desafio/domain/UniqueDigit.java
+++ src/main/java/br/com/desafio/desafio/domain/UniqueDigit.java
@@ -1,10 +1,39 @@
 package br.com.desafio.desafio.domain;
 
 import java.math.BigInteger;
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "unique_digits")
+@AllArgsConstructor
+@NoArgsConstructor
 public class UniqueDigit {
 
+    @Id
+    @Column(name = "id", 
+    nullable = false, 
+    updatable = false, 
+    columnDefinition = "VARCHAR(36)")
+    private UUID id;
+
+    @Column(name = "result", 
+    nullable = false,
+    columnDefinition = "int")
     private int result;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",
+    columnDefinition = "VARCHAR(36)")
+    private User user;
 
     public UniqueDigit(BigInteger number, Integer k) {
         this.result = this.calculateNewUniqueDigit(number, k);
