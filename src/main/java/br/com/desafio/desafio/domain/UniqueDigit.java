e93dd53a21f81704fd11ad6091ee2c765eb293f0
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-12-19 17:25:35 -0300

    feat: logica de calcular uniqueDigit

--- src/main/java/br/com/desafio/desafio/domain/UniqueDigit.java
+++ src/main/java/br/com/desafio/desafio/domain/UniqueDigit.java
@@ -1,5 +1,39 @@
 package br.com.desafio.desafio.domain;
 
+import java.math.BigInteger;
+
 public class UniqueDigit {
 
-}
+    private int result;
+
+    public UniqueDigit(BigInteger number, Integer k) {
+        this.result = this.calculateNewUniqueDigit(number, k);
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    private int calculateNewUniqueDigit(BigInteger number, Integer k) {
+        String numberString = String.valueOf(number)
+        .repeat(k);
+
+        int uniqueDigit = this.sumOfDigits(numberString);
+
+        while(uniqueDigit > 9) {
+            uniqueDigit = this.sumOfDigits(String.valueOf(uniqueDigit));
+        }
+
+        return uniqueDigit;
+    }
+
+    private int sumOfDigits(String numberString) {
+        int sum = 0;
+
+        for (var digit : numberString.toCharArray()) {
+            sum += Character.getNumericValue(digit);
+        }
+
+        return sum;
+    }
+} 
