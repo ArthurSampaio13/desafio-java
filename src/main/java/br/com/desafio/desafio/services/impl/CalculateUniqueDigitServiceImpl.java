e696dc33e044cc56c71aca768ed384ed4208f5e0
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-12-23 21:58:57 -0300

    feat: associando uniqueDigit com usuario

--- src/main/java/br/com/desafio/desafio/services/impl/CalculateUniqueDigitServiceImpl.java
+++ src/main/java/br/com/desafio/desafio/services/impl/CalculateUniqueDigitServiceImpl.java
@@ -7,14 +7,27 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import br.com.desafio.desafio.domain.UniqueDigit;
+import br.com.desafio.desafio.repository.UserRepository;
 import br.com.desafio.desafio.services.CalculteUniqueDigitService;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class CalculateUniqueDigitServiceImpl implements CalculteUniqueDigitService{
+
+    private final UserRepository userRepository;
 
     @Override
     public Integer calculateUniqueDigit(BigInteger number, int k, Optional<UUID> userId) {
         var uniqueDigit = new UniqueDigit(number, k);
+
+        if (userId.isPresent()) {
+            this.userRepository.findById(userId.get()).ifPresent(user -> { 
+                user.addUniqueDigit(uniqueDigit);
+                this.userRepository.save(user);
+            });
+        }
+
         return uniqueDigit.getResult();
     }
     
